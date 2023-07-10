d4f36840b68e8ffa913af0df27f13adee637b184
Sashwat Kumar Verma <125905179+KumarSashwat02@users.noreply.github.com>
2023-07-10 17:44:07 +0530

    Time: 0 ms (100%), Space: 45.1 MB (41.09%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,22 +1,53 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int index = -1;
-        int mid = nums.length/2;
-        if(target == nums[mid]){
-            index = mid;
-        }
-        if(target > nums[mid]){
-            for(int i=mid+1; i<nums.length; i++){
-                if(nums[i] == target)
-                index = i;
-                
+        int s = 0;
+        int e = nums.length-1;
+        while(s<=e){
+            int mid = (s+e)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(target < nums[mid]){
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
             }
         }
-        for(int i=0; i<mid; i++){
-            if(nums[i] == target){
-                index = i;       
-            }
-        }
-        return index;
+        return -1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int index = -1;
+        // int mid = nums.length/2;
+        // if(target == nums[mid]){
+        //     return mid;
+        // }
+        // if(target > nums[mid]){
+        //     for(int i=mid+1; i<nums.length; i++){
+        //         if(nums[i] == target)
+        //         index = i;
+        //     }
+        // }
+        // for(int i=0; i<mid; i++){
+        //     if(nums[i] == target){
+        //         index = i;       
+        //     }
+        // }
+        // return index;
     }
 }

380c85453a9d6ceb193e73061f74818cbc4e5a75
Sashwat Kumar Verma <125905179+KumarSashwat02@users.noreply.github.com>
2023-08-26 05:03:24 +0530

    Time: 0 ms (100%), Space: 42.8 MB (87.42%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,4 +1,5 @@
 class Solution {
+    // by recurssion
     // public int solve(int[] cost, int n ){
     //     if(n==1 || n==0){
     //         return cost[n];
@@ -10,14 +11,33 @@ class Solution {
     //     int n = cost.length-1;
     //     return Math.min(solve(cost,n-1),solve(cost,n));
     // }
+
+    // by memoization
+    // public int solve(int[] cost, int n ,int[] storage){
+    //     if(n==1 || n==0){
+    //         storage[n] = cost[n];
+    //         return storage[n];
+    //     }
+    //     int ans = cost[n] + Math.min(solve(cost,n-1,storage) , solve(cost,n-2,storage));
+    //     storage[n] = ans;
+    //     return storage[n];
+    // }
+    // public int minCostClimbingStairs(int[] cost) {
+    //     int n = cost.length-1;
+    //     int[] storage = new int[n+1];
+    //     return Math.min(solve(cost,n-1,storage),solve(cost,n,storage));
+    // }
+
+
+    // By dynamic programming
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length-1;
-        int ans[] = new int[n+1];
-        ans[0] = cost[0];
-        ans[1] = cost[1];
+        int dp[] = new int[n+1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
         for(int i=2; i<=n; i++){
-            ans[i] = cost[i] + Math.min(ans[i-2],ans[i-1]);
+            dp[i] = cost[i] + Math.min(dp[i-2],dp[i-1]);
         }
-        return Math.min(ans[n],ans[n-1]);
+        return Math.min(dp[n],dp[n-1]);
     }
 }

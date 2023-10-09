f54d41bad3d2aa546965562e95167f39074c508b
Sashwat Kumar Verma <125905179+KumarSashwat02@users.noreply.github.com>
2023-10-09 11:30:51 +0530

    Time: 0 ms (100%), Space: 41 MB (24.9%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -28,11 +28,13 @@ class Solution {
         // digits[digits.length-1] = digits[digits.length-1]+1;
         // return digits;
         for (int i = digits.length - 1; i >= 0; i--) {
-	        if (digits[i] < 9) {
-		    digits[i]++;
-		    return digits;
-	    }
-	    digits[i] = 0;
+	        if (digits[i] == 9){
+		        digits[i] = 0;
+	        }
+            else{
+	            digits[i]++;
+                return digits;
+            }
         }
         digits = new int[digits.length + 1];
         digits[0] = 1;

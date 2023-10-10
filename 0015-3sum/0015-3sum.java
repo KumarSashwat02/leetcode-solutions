d383aa219b3cb3316727b1ec239b3e5429ff2ada
Sashwat Kumar Verma <125905179+KumarSashwat02@users.noreply.github.com>
2023-10-10 14:23:43 +0530

    Time: 27 ms (89.75%), Space: 51.6 MB (18.81%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        HashSet<List<Integer>> ansSet = new HashSet<>();
+        Arrays.sort(nums);
         int n = nums.length;
-        HashSet<Integer> set = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int k = -(nums[i] + nums[j]);
-                if(set.contains(k)){
+            if(i>0 && nums[i] == nums[i-1])continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
                     List<Integer> l = new ArrayList<>();
                     l.add(nums[i]);
                     l.add(nums[j]);
-                    l.add(k);
-                    Collections.sort(l);
-                    ansSet.add(l);
-                }
-                else{
-                    set.add(nums[j]);
+                    l.add(nums[k]);
+                    ans.add(l);
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                    while(j<k && nums[k] == nums[k+1]){
+                        k--;
+                    }
                 }
             }
-            set.clear();
         }
-        List<List<Integer>> ans = new ArrayList<>(ansSet);
         return ans;
 
 
@@ -30,6 +41,38 @@ class Solution {
 
 
 
+
+// better way but not much optmised
+        // HashSet<List<Integer>> ansSet = new HashSet<>();
+        // int n = nums.length;
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         int k = -(nums[i] + nums[j]);
+        //         if(set.contains(k)){
+        //             List<Integer> l = new ArrayList<>();
+        //             l.add(nums[i]);
+        //             l.add(nums[j]);
+        //             l.add(k);
+        //             Collections.sort(l);
+        //             ansSet.add(l);
+        //         }
+        //         else{
+        //             set.add(nums[j]);
+        //         }
+        //     }
+        //     set.clear();
+        // }
+        // List<List<Integer>> ans = new ArrayList<>(ansSet);
+        // return ans;
+
+
+
+
+
+
+
+// BRUTE FORCE METHOD (WILL GIVE TLE)   
         // HashSet<List<Integer>> set = new HashSet<>();
         // int n = nums.length;
         // for(int i=0; i<n; i++){

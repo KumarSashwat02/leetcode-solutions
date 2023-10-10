fa417031b341445cdb9466df2f009da49e135cc9
Sashwat Kumar Verma <125905179+KumarSashwat02@users.noreply.github.com>
2023-10-10 14:03:42 +0530

    Time: 816 ms (6.45%), Space: 53.6 MB (5.52%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,48 +1,52 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Sort the array
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found a triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    // Move the pointers
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    // Sum is less than zero, increment j to increase the sum
-                    j++;
-                } else {
-                    // Sum is greater than zero, decrement k to decrease the sum
-                    k--;
+        HashSet<List<Integer>> ansSet = new HashSet<>();
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int k = -(nums[i] + nums[j]);
+                if(set.contains(k)){
+                    List<Integer> l = new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[j]);
+                    l.add(k);
+                    Collections.sort(l);
+                    ansSet.add(l);
+                }
+                else{
+                    set.add(nums[j]);
                 }
             }
+            set.clear();
         }
+        List<List<Integer>> ans = new ArrayList<>(ansSet);
         return ans;
+
+
+
+
+
+
+
+
+        // HashSet<List<Integer>> set = new HashSet<>();
+        // int n = nums.length;
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         for(int k=j+1; k<n; k++){
+        //             if(nums[i]+nums[j]+nums[k] == 0){
+        //                 List<Integer> l = new ArrayList<>();
+        //                 l.add(nums[i]);
+        //                 l.add(nums[j]);
+        //                 l.add(nums[k]);
+        //                 Collections.sort(l);
+        //                 set.add(l);
+        //             }
+        //         }
+        //     }
+        // }
+        // List<List<Integer>> ans = new ArrayList<>(set);
+        // return ans;
     }
 }

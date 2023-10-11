4014958587117259d04f76badaf5db20085d630b
Sashwat Kumar Verma <125905179+KumarSashwat02@users.noreply.github.com>
2023-10-11 12:29:13 +0530

    Time: 16 ms (74.19%), Space: 44 MB (46.92%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,30 +1,76 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-        HashSet<List<Integer>> ansSet = new HashSet<>();
-        HashSet<Long> set = new HashSet<>();
+        Arrays.sort(nums);
         for(int i=0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1])continue;
             for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    long sum = nums[i] + nums[j];
+                if(j>i+1 && nums[j] == nums[j-1])continue;
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    long sum = nums[i];
+                    sum += nums[j];
                     sum += nums[k];
-                    long val = target -sum;
-                    if(set.contains(val)){
-                        List<Integer> l = new ArrayList<>();
-                        l.add(nums[i]);
-                        l.add(nums[j]);
-                        l.add(nums[k]);
-                        l.add((int)val);
-                        Collections.sort(l);
-                        ansSet.add(l);
+                    sum += nums[l];
+                    if( sum < target){
+                        k++;
+                    }
+                    else if(sum > target){
+                        l--;
                     }
                     else{
-                        set.add((long)nums[k]);
-                    }                    
+                        List<Integer> l1 = new ArrayList<>();
+                        l1.add(nums[i]);
+                        l1.add(nums[j]);
+                        l1.add(nums[k]);
+                        l1.add(nums[l]);
+                        ans.add(l1);
+                        k++;
+                        l--;
+                    while(k<l && nums[k] == nums[k-1]){
+                        k++;
+                    }
+                    while(k<l && nums[l] == nums[l+1] ){
+                        l--;
+                    }
+                    }
                 }
-                set.clear();
             }
         }
+        return ans;
+
+
+
+
+
+        // HashSet<List<Integer>> ansSet = new HashSet<>();
+        // HashSet<Long> set = new HashSet<>();
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         for(int k=j+1; k<n; k++){
+        //             long sum = nums[i] + nums[j];
+        //             sum += nums[k];
+        //             long val = target -sum;
+        //             if(set.contains(val)){
+        //                 List<Integer> l = new ArrayList<>();
+        //                 l.add(nums[i]);
+        //                 l.add(nums[j]);
+        //                 l.add(nums[k]);
+        //                 l.add((int)val);
+        //                 Collections.sort(l);
+        //                 ansSet.add(l);
+        //             }
+        //             else{
+        //                 set.add((long)nums[k]);
+        //             }                    
+        //         }
+        //         set.clear();
+        //     }
+        // }
+        // List<List<Integer>> ans = new ArrayList<>(ansSet);
+        // return ans;
 
         
         // HashSet<List<Integer>> set = new HashSet<>();
@@ -45,7 +91,7 @@ class Solution {
         //         }
         //     }
         // }
-        List<List<Integer>> ans = new ArrayList<>(ansSet);
-        return ans;
+        // List<List<Integer>> ans = new ArrayList<>(set);
+        // return ans;
     }
 }

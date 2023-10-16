f3f0ee5983c8fbdda68665a244f60ca757cc2583
Sashwat Kumar Verma <125905179+KumarSashwat02@users.noreply.github.com>
2023-10-16 12:49:34 +0530

    Time: 0 ms (100%), Space: 44.9 MB (9.24%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,25 +2,64 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int[] row = new int[n];
-        int[] col = new int[m];
-
+        
+// Optimal Solution and doing it inplace
+        boolean fr = false, fc = false;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
+                    if(i == 0) fr = true;
+                    if(j == 0) fc = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] != 0  && (row[i] == 1 || col[j] == 1)) {
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
+        if(fr){
+            for(int j=0; j<m; j++ ){
+                matrix[0][j] = 0;
+            }
+        }
+        if(fc){
+            for(int i=0; i<n; i++){
+                matrix[i][0] = 0;
+            }
+        }
 
+
+
+//Better Solution but it will increase the space complexity
+        // int[] row = new int[n];
+        // int[] col = new int[m];
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if(matrix[i][j] == 0){
+        //             row[i] = 1;
+        //             col[j] = 1;
+        //         }
+        //     }
+        // }
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if(matrix[i][j] != 0  && (row[i] == 1 || col[j] == 1)) {
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+
+
+
+
+
+// Brute Force Solution but it will have a TC : (n*m)(n+m)+(n*m)
         // for(int i=0; i<n; i++){
         //     for(int j=0; j<m; j++){
         //         if(matrix[i][j] == 0){

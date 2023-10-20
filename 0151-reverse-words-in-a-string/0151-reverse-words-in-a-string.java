fffed00a834a9062cb16c8cc48ddc15f2e8f4100
Sashwat Kumar Verma <125905179+KumarSashwat02@users.noreply.github.com>
2023-10-21 01:13:20 +0530

    Time: 8 ms (59.91%), Space: 41 MB (99.09%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,25 +1,51 @@
 class Solution {
     public String reverseWords(String s) {
-        Stack<String> st = new Stack<>();
-        String s1 = "";
-        StringBuilder ans = new StringBuilder();
+        StringBuilder res = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
 
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == ' '){
-                if(s1.length()>0){
-                    st.push(s1);
-                    s1 = "";
+        for (int i = 0; i < s.length(); ++i) {
+            while (i < s.length() && s.charAt(i) != ' ') {
+                temp.append(s.charAt(i));
+                ++i;
+            }
+
+            if (temp.length() != 0) {
+                // If we find a word we will add it at beginning of result.
+                if (res.length() != 0) {
+                    res.insert(0, " ");
+                    res.insert(0, temp);
+                } else {
+                    res.append(temp);
                 }
-            }
-            else{
-                s1 = s1 + s.charAt(i);
+                temp.setLength(0);
             }
         }
-        ans.append(s1);
-        while(st.size()>0){
-            ans.append(" ").append(st.pop());
-        }
-        return ans.toString().trim();
+        return res.toString();
+
+
+
+
+
+    //     Stack<String> st = new Stack<>();
+    //     String s1 = "";
+    //     StringBuilder ans = new StringBuilder();
+
+    //     for(int i=0; i<s.length(); i++){
+    //         if(s.charAt(i) == ' '){
+    //             if(s1.length()>0){
+    //                 st.push(s1);
+    //                 s1 = "";
+    //             }
+    //         }
+    //         else{
+    //             s1 = s1 + s.charAt(i);
+    //         }
+    //     }
+    //     ans.append(s1);
+    //     while(st.size()>0){
+    //         ans.append(" ").append(st.pop());
+    //     }
+    //     return ans.toString().trim();
     }
 }
 
